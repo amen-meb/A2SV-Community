bfd31e6af25801ca73a48f745ea02fb94f83b166
amen-meb <amanuelmebratu1@gmail.com>
2024-06-24 18:43:09 +0300

    Update listComperhension.py

--- listComperhension.py
+++ listComperhension.py
@@ -1,1 +1,11 @@
-def 
+def __name__ == '__init__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+
+    coordinates = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if (i + j + k) != n]
+
+
+    print(coordinates)
